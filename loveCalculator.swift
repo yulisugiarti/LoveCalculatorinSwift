866ea8f0decb5d4bd3f481d971f736a6bb503b56
Yuli Sugiarti <102387475+yulisugiarti@users.noreply.github.com>
2024-04-13 08:02:20 +0700

    Create loveCalculator.swift
    #Function #Conditional #Randomization

---
+++ loveCalculator.swift
@@ -0,0 +1,11 @@
+func loveCalculator() {
+    let score = Int.random(in: 1...100)
+    if score == 100{
+        print("You Love Each Other Like Kanye Loves Kanye")
+    }
+    else{
+        print("You'll be Forever Alone")
+    }
+}
+
+loveCalculator()
