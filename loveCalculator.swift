dd04eee19fed6f41f05c9b23c559390357099c6c
Yuli Sugiarti <102387475+yulisugiarti@users.noreply.github.com>
2024-04-13 08:16:56 +0700

    Update loveCalculator.swift

--- loveCalculator.swift
+++ loveCalculator.swift
@@ -1,6 +1,6 @@
 func loveCalculator() {
     let score = Int.random(in: 1...100)
-    if score >= 80{
+    if score > 80{
         print("You Love Each Other Like Kanye Loves Kanye")
     }
     else if score >= 40 && score <= 80{
